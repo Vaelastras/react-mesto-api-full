83e8cc5e3a5cc39794ba5340467790ba558361e9
Alexey Belov <vaelastras@gmail.com>
2020-11-27 19:44:14 +0300

    prerelease version

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -145,5 +145,5 @@ export const api = new Api({
   // baseUrl:'https://mesto.nomoreparties.co/v1/cohort-14',
   // baseUrl:'http://api.sealkindom.students.nomoreparties.xyz',
   // baseUrl:'http://localhost:3000'
-  baseUrl: `${window.location.protocol}${process.env.API_URL}`
+  baseUrl: `${window.location.protocol}${process.env.API_URL || '//localhost:3000'}`
 });
